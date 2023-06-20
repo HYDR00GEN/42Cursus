b2f984041af3793c7624cb78fe056026c07a7576
HYDR00GEN <ovy.hydrogen@gmail.com>
2023-06-20 15:17:40 +0200

    idk

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -5,24 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: opaunas <opaunas@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/19 07:48:10 by opaunas           #+#    #+#             */
+/*   Created: 2023/06/10 01:01:19 by opaunas           #+#    #+#             */
 /*   Updated: 2023/06/19 07:48:10 by opaunas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *str, int c, size_t n)
+void    *ft_memset(void *s, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	*ptr;
-
-	i = 0;
-	ptr = (unsigned char *) str;
-	while (i < n)
-	{
-		ptr[i] = (unsigned char) c;
-		i++;
-	}
-	return ((void *)str);
+    unsigned char *str;
+    
+    str = (unsigned char *)s;
+    while (n--)
+        *str++ = (unsigned char)c;
+    return (s);
 }
