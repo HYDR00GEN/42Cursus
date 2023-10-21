af0c223a34e21e873f1bf30f9d0120472ff91edf
HYDR00GEN <ovy.hydrogen@gmail.com>
2023-10-21 12:29:13 +0200

    insert bonus from other folder

--- getNextLine/get_next_line_bonus.c
+++ getNextLine/get_next_line_bonus.c
@@ -9,3 +9,32 @@
 /*   Updated: 2023/10/15 08:36:50 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_len(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*out;
+	static char	*str[4096];
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	str[fd] = ft_line_allocation(fd, str[fd]);
+	if (!str[fd])
+		return (NULL);
+	out = ft_next_line(str[fd]);
+	str[fd] = ft_rem_line(str[fd]);
+	return (out);
+}
--- getNextLine/get_next_line_bonus.h
+++ getNextLine/get_next_line_bonus.h
@@ -10,3 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+size_t	ft_len(char *str);
+char	*ft_strchr(char *str, int c);
+char	*ft_next_line(char *str);
+char	*ft_rem_line(char *line);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_line_allocation(int fd, char *str);
+char	*get_next_line(int fd);
+
+# endif
